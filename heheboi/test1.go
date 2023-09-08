c6225226aade64e4de768a6d9b9968513c44ce91
VuKer <74062885+TDMVu18@users.noreply.github.com>
2023-09-08 10:55:53 +0700

    Create test1.go

---
+++ heheboi/test1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home page"))
+}
+
+func aboutHandle(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Giới thiệu"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homePage) //đăng ký router "/" với hàm xử lý homeHandle
+	//http.HandleFunc("/about", aboutHandle) //đăng ký router "/about" với hàm xử lý aboutHandle
+	fmt.Println("Server listenning on port 3000 ...")
+	fmt.Println(http.ListenAndServe(":3000", mux)) //tạo server, hàm ListenAnhServe nhận yêu cầu
+}
